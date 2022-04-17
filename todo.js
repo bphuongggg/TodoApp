6546451e86cbd097e61a1dddb9978b0ae21ad1bc
bphuongggg <80316539+bphuongggg@users.noreply.github.com>
2022-04-17 14:28:53 +0700

    update todo count and check todo item

--- todo.js
+++ todo.js
@@ -1,17 +1,24 @@
 const todoList = document.querySelector("ul");
+const todoItem= document.querySelector("li");
+const todoCount = document.querySelector(".todo-count strong");
 function addNewTodo(event) {
   if (event.keyCode == 13) {
     var todo = document.getElementById("new-todo");
     console.log(todo.value);
 
     createNewTodoElement(todo.value);
+    setCountTodo(1);
     document.getElementById("toggle-icon").style.display="block"
     document.getElementById("main").style.display="block";
     document.getElementById("footer").style.display="block";
    todo.value = "";
   }
 }
-
+//update todo item
+function setCountTodo(number){
+  todoCount.innerText = +todoCount.innerText + number;
+}
+// create list todo
 function createNewTodoElement(text){
   
    var todoElement = document.createElement("li");
@@ -23,6 +30,7 @@ function createNewTodoElement(text){
 
     todoView.className = "view";
     todoCheck.className = "toggle";
+    todoCheck.type="checkbox";
     todoDelete.className = "destroy";
     todoEdit.className = "edit";
 
@@ -36,12 +44,33 @@ function createNewTodoElement(text){
    
    todoList.appendChild(todoElement);
 }
-
+// remove todo item
 todoList.addEventListener('click', (event) => {
-  console.log(event.target.className = 'destroy');
-  if (event.target.className== 'destroy') {
+  
+  if (event.target.classList.contains('destroy')) {
     const myParent = event.target.parentElement;
     myParent.parentElement.remove();
+    setCountTodo(-1);
+    document.getElementById("footer").style.display="block";
   }
+
+  if(event.target.classList.contains('toggle')){
+    var checkInput = event.target;
+    var completedInput = event.target.parentElement;
+    
+    if(checkInput.checked == true){
+      completedInput.parentElement.classList.add("completed");
+      setCountTodo(-1);
+    }
+    else{
+      completedInput.parentElement.classList.remove("completed");
+      setCountTodo(1);
+    }
+    console.log(checkInput.checked);
+  }
+  
 })
+
+//check todo completed item
+
 
